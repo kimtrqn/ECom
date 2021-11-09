9f19905c3a5707e73f1788f59346afab433486dc
Julie Tran <kimtran767@gmail.com>
2021-11-09 12:55:58 -0800

    added shop component with another files of data to added to shop

---
+++ src/components/shop/shop.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import SHOP_DATA from './shop-data';
+
+import '../stylesheets/shop-style.scss';
+
+class ShopPage extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            collections: [
+
+            ]
+        }
+    }
+
+};
+
+export default ShopPage;
