2c56b51b9c11a18c452be7c69545ad3c316c56ca
Julie Tran <kimtran767@gmail.com>
2021-12-09 12:29:04 -0800

    change appjs to class component to keep state for user authencation as wheather a user is sign in or not

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import { auth } from './firebase/firebase.utils';
+
 import Header from './components/header/header';
 import Home from './pages/homepage/Home';
 import Shop from './pages/shop/Shop';
@@ -8,17 +10,46 @@ import Session from './pages/session/Session';
 
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    <Header />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/shop' component={Shop} />
-        <Route path='/signin' component={Session} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  };
+
+  // it equal null because it sign out 
+  unsubscribeFromAuth = null;
+
+  // set unsubscribeFromAuth to a new method to set the user 
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user})
+
+      console.log(user._delegate);
+    });
+  }
+
+  // it setting it back to null so there is no user left
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  
+  render() {
+    return (
+      <div>
+      <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/shop' component={Shop} />
+          <Route path='/signin' component={Session} />
+        </Switch>
+      </div>
+    )
+  };
 }
 
 export default App;
