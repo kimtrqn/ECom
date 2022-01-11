a57c7e24a63e73f499fce261e8f1d8a279a4fac6
Julie Tran <kimtran767@gmail.com>
2022-01-11 13:27:32 -0800

    added item to checkout success

--- src/components/cart-drop/cart-drop.jsx
+++ src/components/cart-drop/cart-drop.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Button from '../button/button';
+import CartItem from '../cart-item/cart-item';
 
 import './cart-drop-style.scss';
 
-const CartDropDown = () => (
+const CartDropDown = ({ cartItems }) => (
     <div className="cart-dropdown">
         <div className="cart-items">
-            <Button>GO TO CHECKOUT</Button>
+            {
+                cartItems.map(cartItem => 
+                    <CartItem key={cartItem.id} item={cartItem} />
+                )
+            }
         </div>
+        <Button>GO TO CHECKOUT</Button>
     </div>
 );
 
-export default CartDropDown;
+// export default CartDropDown;
+
+const mstp = ({ cart: { cartItems }}) => ({
+    cartItems
+});
+
+export default connect(mstp)(CartDropDown);
