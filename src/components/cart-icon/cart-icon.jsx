5cbc1438d30553c53885bf1cdc3cb3499d41459f
Julie Tran <kimtran767@gmail.com>
2022-01-11 13:39:36 -0800

    fixed quantity typo errors added cart item quanity number

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -6,10 +6,10 @@ import { toggleCartHidden } from '../../redux/cart/cart_actions';
 import './cart-icon-style.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/images/shopping-bag.svg';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon' />
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 );
 
@@ -17,4 +17,8 @@ const mdtp = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden()),
 })
 
-export default connect(null, mdtp)(CartIcon);
+const mstp = ({ cart : { cartItems }}) => ({
+    itemCount: cartItems.reduce((accQuantity, cartItem) => accQuantity + cartItem.quantity, 0)
+})
+
+export default connect(mstp, mdtp)(CartIcon);
